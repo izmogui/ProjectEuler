62a9b3a199b5b88330c8e43934e36fc5846a4dd6
izmogui <gimrstcrc@gmail.com>
2017-10-26 15:58:52 +0200

    empty_p16

--- p16.py
+++ p16.py
@@ -2,15 +2,6 @@ import sys
 from operator import mul
 import time
 
-def factorial(num):
-    """Iteratively calculate num!."""
-    return reduce(mul, xrange(1, num+1)) if num else 1
-
-def fact(num):
-	if num == 0 or num == 1:
-		return 1
-	else:
-		return num * fact(num - 1)
 
 def main():
 	
@@ -18,12 +9,7 @@ def main():
 	#millisec
 	start = int(round(time.time() * 1000))
 	
-	rows = 20
-	columns = 20
-
-	print factorial(rows+columns)
-	print fact (rows + columns)
-
+	#foo
 	
 	print "Elapsed Time: ", int(round(time.time() * 1000)) - start, "milliseconds"
 
