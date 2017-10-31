ee9d416e541b275e0d33f1f31c28953881cac2e1
izmogui <gimrstcrc@gmail.com>
2017-10-31 10:24:31 +0100

    solved

--- p16.py
+++ p16.py
@@ -1,15 +1,38 @@
-import sys
-from operator import mul
 import time
 
-
 def main():
-	
 	
 	#millisec
 	start = int(round(time.time() * 1000))
 	
-	#foo
+	exponent = 1000
+	
+	digits = [1]
+	aux_sum = 0
+	
+	# rounds
+	for i in range(0, exponent):	
+		# in each round iterate over all cells
+		for j in range (0, len(digits)):
+			# cell = 2*cell + carry
+			aux_sum = digits[j] + digits[j] + aux_sum
+			# carry overflow
+			if aux_sum > 9:
+				digits[j] = aux_sum % 10
+				# case carry overflows the list
+				if len(digits) <= j + 1:
+					# increment list size by 1 and set carry
+					digits.append(1)
+					aux_sum = 0
+				else:
+					# set carry for the next iteration
+					aux_sum = 1
+			else:
+				digits[j] = aux_sum
+				aux_sum = 0
+
+	#print digits
+	print "Sum: ", sum(digits)
 	
 	print "Elapsed Time: ", int(round(time.time() * 1000)) - start, "milliseconds"
 
